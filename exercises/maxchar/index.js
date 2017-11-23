7a1a9db74f5169f33c383b93c8ba93bb67de38f0
ScottRudiger <scottrudiger@gmail.com>
2017-11-22 23:06:22 -0800

    initial solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,7 +6,18 @@
 // maxChar("apple 1231111") === "1"
 
 const maxChar = str => {
-
+  const counts = [...str].reduce((counts, char) => (
+    counts[char] ? counts[char]++ : counts[char] = 1,
+    counts
+  ), {});
+  let maxC, maxN = 0;
+  for (let count in counts) {
+    if (counts[count] > maxN) {
+      maxN = counts[count];
+      maxC = count;
+    }
+  }
+  return maxC;
 };
 
 module.exports = maxChar;
