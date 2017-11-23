cf79066d954256699b03e796a104896a02231377
ScottRudiger <scottrudiger@gmail.com>
2017-11-22 23:46:51 -0800

    alternative solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,13 +12,22 @@
 //   4
 //   buzz
 
-const fizzBuzz = n => {
-  for (i = 1; i <= n; i++) {
+fizzBuzz = n => {
+  for (let i = 1; i <= n; i++) {
     let result = '';
     if (!(i % 3)) result += 'fizz';
     if (!(i % 5)) result += 'buzz';
     if (i % 3 && i % 5) result = i;
     console.log(result);
+  }
+};
+
+fizzBuzz = n => {
+  for (let i = 1; i <= n; i++) {
+    if (!(i % 3) && !(i % 5)) console.log('fizzbuzz');
+    else if (!(i % 3)) console.log('fizz');
+    else if (!(i % 5)) console.log('buzz');
+    else console.log(i);
   }
 };
 
