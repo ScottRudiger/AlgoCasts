75a7a619a2e0ac5f553615cc526c79f0ad43305d
ScottRudiger <scottrudiger@gmail.com>
2017-11-25 17:20:02 -0800

    alternative solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -21,4 +21,13 @@ capitalize = str => {
   return result;
 };
 
+capitalize = str => {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') result += str[i].toUpperCase();
+    else result += str[i];
+  }
+  return result;
+};
+
 module.exports = capitalize;
