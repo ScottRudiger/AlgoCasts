320fc46399356405f72c4fa024cb11ffbad19090
ScottRudiger <scottrudiger@gmail.com>
2017-11-25 19:51:38 -0800

    alternative solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -31,4 +31,15 @@ steps = n => {
   }
 };
 
+steps = n => {
+  for (let i = 0; i < n; i++) {
+    let step = '';
+    for (let j = 0; j < n; j++) {
+      if (j <= i) step += '#';
+      else step += ' ';
+    }
+    console.log(step);
+  }
+};
+
 module.exports = steps;
