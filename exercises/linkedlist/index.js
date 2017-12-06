2200b3ea569f1bbde178f657380297c0469cd337
ScottRudiger <scottrudiger@gmail.com>
2017-12-05 17:49:09 -0800

    implement getLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -12,18 +12,23 @@ class Node {
 class LinkedList {
   constructor() {
     this.head = null;
+    this.tail = null;
     this._size = 0;
   }
   insertFirst(data) {
     const newHead = new Node(data, this.head);
     this.head = newHead;
     this._size++
+    if (!this.tail) this.tail = newHead;
   }
   size() {
     return this._size;
   }
   getFirst() {
     return this.head;
+  }
+  getLast() {
+    return this.tail;
   }
 }
 
