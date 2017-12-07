262c717a7bb2ca1e9af874dca7d368f7ad23dd8e
ScottRudiger <scottrudiger@gmail.com>
2017-12-06 22:02:57 -0800

    refactor insertFirst method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -127,6 +127,9 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
 }
 
 module.exports = { Node, LinkedList };
