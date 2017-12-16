2e5a41c5ffa3a7adf6f83f380e062f333bfd9ce6
ScottRudiger <scottrudiger@gmail.com>
2017-12-16 09:25:12 -0800

    initial solution

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -13,7 +13,12 @@
 //   midpoint(l); // returns { data: 'b' }
 
 const midpoint = list => {
-
+  let slow = fast = list.head;
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
 };
 
 module.exports = midpoint;
